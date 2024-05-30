7dc882d993a95e68ccf08015e5b89bbf23f91995
Dp_Botz <149217129+Dhineshpavi@users.noreply.github.com>
2024-05-30 02:11:16 -0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/MOGAMBO-XD/File-Sharing-Bot'>Click here</a>\n○ Channel : @Cybermusicproject\n○ Support Group : @CyberSupportGroup</b>",
+            text = f"<b>■ 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿 : <a href='https://t.me/dpowner_bot'>𝗜𝘁'𝘀 𝗠𝗲</a>\n■ 𝗟𝗮𝗻𝗴𝘂𝗮𝗴𝗲 : <code>𝗣𝘆𝘁𝗵𝗼𝗻𝟯</code>\n■ 𝗦𝗼𝘂𝗿𝗰𝗲 𝗖𝗼𝗱𝗲 : <a href='https://t.me/DP_BOTZ'>𝗖𝗹𝗶𝗰𝗸</a>\n■ 𝗠𝗼𝘃𝗶𝗲𝘀 : <a href='https://t.me/movies_days'>𝗧𝗮𝗺𝗶𝗹 𝗠𝗼𝘃𝗶𝗲𝘀</a>\n■ 𝗔𝗻𝗶𝗺𝗲𝘀  : <a href='https://t.me/Animetoon_xyz'>𝗧𝗮𝗺𝗶𝗹 𝗔𝗻𝗶𝗺𝗲𝘀</a>\n■ 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 : <a href='https://t.me/AnimeToon_Support_Bot'>𝗖𝗹𝗶𝗰𝗸</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
