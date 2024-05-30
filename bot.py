93600c89d8d6176236f04cd3b311f4578141b50f
Dp_Botz <149217129+Dhineshpavi@users.noreply.github.com>
2024-05-30 02:26:07 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -59,7 +59,7 @@ class Bot(Client):
         self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/All_Tamil_movies_request")
         self.LOGGER(__name__).info(f""" \n\n       
-        MOGAMBO 
+        D♥P♥_B♥O♥T♥Z♥ 
                                           """)
         self.username = usr_bot_me.username
         #web-response
