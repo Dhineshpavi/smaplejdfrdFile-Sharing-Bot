e08bf25c66ce5d0e388d4db7168d57096f1f88d8
Dp_Botz <149217129+Dhineshpavi@users.noreply.github.com>
2024-05-30 01:39:38 -0700

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://evamusicbot:mrwaris04@clu
 DB_NAME = os.environ.get("DATABASE_NAME", "DPBOTZDEVELOPER")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "DP_BOTZ"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002008853384"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
